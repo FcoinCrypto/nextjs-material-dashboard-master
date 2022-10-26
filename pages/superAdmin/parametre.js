4fe58825729b70113d72a4d0379e3197c41b7c40
manoa11 <manoaharinjo@gmail.com>
2022-10-26 17:23:32 +0300

    parametre

--- pages/superAdmin/parametre.js
+++ pages/superAdmin/parametre.js
@@ -3,6 +3,11 @@ import { Button, Grid, InputLabel } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
 import { getCours, updateCours } from "../../services/wallet";
 import superAdmin from "layouts/superAdmin.js";
+import { Card } from "@mui/material";
+import { Typography } from "antd";
+import CurrencyFormat from 'react-currency-format';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const useStyles = makeStyles({
     root: {
@@ -55,7 +60,7 @@ export default function parametre() {
     const classes = useStyles();
     const [cours, setCours] = useState();
     const [amount, setAmount] = useState();
-    const [devis, setDevis] = useState("ftc");
+    const [devis, setDevis] = useState("USDT");
 
     const onDevisChange = (event, value) => {
         setDevis(event.target.value);
@@ -69,9 +74,13 @@ export default function parametre() {
     }
 
     const changeCours = async (e) => {
-        console.log(devis, amount)
         const response = await updateCours(devis, amount);
-        console.log(response);
+        if(response.statusText == "OK"){
+            toast.success("cours mise a jours en " + devis.toUpperCase());
+            window.location.reload(false);
+        }else{
+            toast.success("erreur" + response.statusText);
+        }
     }
 
 
@@ -79,7 +88,7 @@ export default function parametre() {
         if (!cours) {
             const data = await getCours();
             const cours = Object.entries(data.data[0].attributes).filter(row => {
-                return row.includes("ftc") || row.includes("ar") || row.includes("eth") || row.includes("monero") || row.includes("usdt") || row.includes("btc") || row.includes("euro");
+                return  row.includes("ar") || row.includes("eth") || row.includes("monero") || row.includes("usdt") || row.includes("btc") || row.includes("euro");
             }
             );
             setCours(cours);
@@ -90,45 +99,57 @@ export default function parametre() {
     return (
         <div>
             { cours &&
-            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                <Grid item xs={12}>
-                    <InputLabel htmlFor="input-with-icon-adornment">
-                        Changer les cours
-                    </InputLabel>
-                    <br/>
-                    <div className={classes.group}>
-                        {/* <img 
-                            className="mx-3" 
-                            src={props.linkImage}
-                            style={{width:32,backgroundColor:'white',borderRadius:50,marginTop:10,borderRight:'solid 1px white'}} alt="Facebook image" 
-                        /> */}
-                        <input 
-                            className={classes.input} 
-                            type="text"
-                            value={amount} 
-                            onChange={ev => onAmountChange(ev.target.value)}
-                            onFocus = {(e) => handleFocus(e)}
-                        />
-                        <select className={classes.select} value={devis} onChange={onDevisChange}>
-                            {cours.map((cour => (
-                                <option className={classes.option} value={cour[0]}>{cour[0]}</option>
-                        )))}
-                        </select>
-                        </div>
+            <>
+                <Card>
+                { cours.map((cour => (
+                    <Grid item lg={4} md={4} xs={12}>
+                    <Typography sx={{ mb: 1.5 }}  color="text.secondary">
+                        <CurrencyFormat value={cour[1]} displayType={'text'} thousandSeparator={'.'} decimalSeparator={','} prefix={'1 FTC =  '} suffix={' ' +cour[0]}  />
+                    </Typography>
                 </Grid>
+                )))}    
+                </Card>
+                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                    <Grid item xs={12}>
+                        <InputLabel htmlFor="input-with-icon-adornment">
+                            Changer les cours
+                        </InputLabel>
+                        <br/>
+                        <div className={classes.group}>
+                            {/* <img 
+                                className="mx-3" 
+                                src={props.linkImage}
+                                style={{width:32,backgroundColor:'white',borderRadius:50,marginTop:10,borderRight:'solid 1px white'}} alt="Facebook image" 
+                            /> */}
+                            <input 
+                                className={classes.input} 
+                                type="text"
+                                value={amount} 
+                                onChange={ev => onAmountChange(ev.target.value)}
+                                onFocus = {(e) => handleFocus(e)}
+                            />
+                            <select className={classes.select} value={devis} onChange={onDevisChange}>
+                                {cours.map((cour => (
+                                    <option className={classes.option} value={cour[0]}>{cour[0].toUpperCase()}</option>
+                            )))}
+                            </select>
+                            </div>
+                    </Grid>
 
-                <Button
-                    variant="contained" 
-                    color="primary" 
-                    // disabled={btnDisabled} 
-                    type="submit"
-                    onClick={() => changeCours()}
-                    
-                >
-                    Modifier
-                </Button>
-            </Grid>
+                    <Button
+                        variant="contained" 
+                        color="primary" 
+                        // disabled={btnDisabled} 
+                        type="submit"
+                        onClick={() => changeCours()}
+                        
+                    >
+                        Modifier
+                    </Button>
+                </Grid>
+            </>
             }
+            <ToastContainer />
         </div>
     );
     
